bc5ce718a9ae5fed8e049ad67f8f0c0cd9fa96aa
angemms <korabelnikov08@yandex.ru>
2026-04-19 23:52:12 +0300

    Tray Fix

--- lib/services/tray_service.dart
+++ lib/services/tray_service.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:tray_manager/tray_manager.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:lizaplayer/services/player_service.dart';
 
 class TrayService with TrayListener, WindowListener {
   static final TrayService _instance = TrayService._internal();
   factory TrayService() => _instance;
   TrayService._internal();
+
+  final PlayerService _playerService = PlayerService();
+  StreamSubscription? _trackSub;
+  StreamSubscription? _playSub;
 
   Future<void> init() async {
     trayManager.addListener(this);
@@ -16,8 +22,41 @@ class TrayService with TrayListener, WindowListener {
       Platform.isWindows ? 'assets/logo.png' : 'assets/logo.png',
     );
     
+    await _updateMenu();
+
+    _trackSub?.cancel();
+    _trackSub = _playerService.trackStream.listen((_) => _updateMenu());
+    _playSub?.cancel();
+    _playSub = _playerService.playingStream.listen((_) => _updateMenu());
+  }
+
+  Future<void> _updateMenu() async {
+    final track = _playerService.currentTrack;
+    final isPlaying = _playerService.playing;
+
     final menu = Menu(
       items: [
+        if (track != null) ...[
+          MenuItem(
+            key: 'current_track',
+            label: '${track.title} - ${track.artistName}',
+            disabled: true,
+          ),
+          MenuItem.separator(),
+        ],
+        MenuItem(
+          key: 'prev_track',
+          label: 'Previous',
+        ),
+        MenuItem(
+          key: 'toggle_play',
+          label: isPlaying ? 'Pause' : 'Play',
+        ),
+        MenuItem(
+          key: 'next_track',
+          label: 'Next',
+        ),
+        MenuItem.separator(),
         MenuItem(
           key: 'show_window',
           label: 'Show',
@@ -45,11 +84,27 @@ class TrayService with TrayListener, WindowListener {
 
   @override
   void onTrayMenuItemClick(MenuItem menuItem) {
-    if (menuItem.key == 'show_window') {
-      windowManager.show();
-      windowManager.focus();
-    } else if (menuItem.key == 'exit_app') {
-      exit(0);
+    switch (menuItem.key) {
+      case 'show_window':
+        windowManager.show();
+        windowManager.focus();
+        break;
+      case 'exit_app':
+        exit(0);
+        break;
+      case 'toggle_play':
+        if (_playerService.playing) {
+          _playerService.pause();
+        } else {
+          _playerService.play();
+        }
+        break;
+      case 'next_track':
+        _playerService.next();
+        break;
+      case 'prev_track':
+        _playerService.previous();
+        break;
     }
   }
 
